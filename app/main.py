630fbbf6fe01a39f93d411e82fb3a27c90c3086f
t-shiba-314 <t.314shiba@gmail.com>
2024-08-06 12:19:07 +0900

    update template

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, APIRouter
 from database import engine, Base
-from routers import user
+# routing example
+# from routers import user
 
 # api
 app = FastAPI()
 api_router = APIRouter(prefix='/api')
-api_router.include_router(user.router)
+# routing example
+# api_router.include_router(user.router)
 
 app.include_router(api_router)
 
